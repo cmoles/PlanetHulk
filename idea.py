8a0b7a1ba3af12c32bf383f7c78c0d48f6af8e00
Chris Moles <cmoles@picobook.(none)>
2010-02-25 01:40:20 -0800

    Committer: Chris Moles <cmoles@picobook.(none)>
     On branch animate
     Changes to be committed:
    
    	modified:   idea.py

--- idea.py
+++ idea.py
@@ -1,10 +1,27 @@
 # Characters - Hulk, Spider-man, Captain America, Dr. Doom
     # Animations - Walk, Run, Fly, Fight
     # Health
-    
+import os 
+import pygame
+from pygame.locals import *
+from random import randint
+from hulk import hulk_data
+
+RESOLUTION = (640, 480)
+
+pygame.init()
+screen       = pygame.display.set_mode(RESOLUTION)
+background   = pygame.Surface(RESOLUTION)
+default_font = pygame.font.get_default_font()
+font         = pygame.font.SysFont(default_font, 20, False)    
+
+background.fill((255,175,175))
+
+screen.blit(background, (0, 0))
+
 class Animation():
-    def __init__(self,images,offsets,fps=10):
-        self._images        = images
+    def __init__(self,images,offsets,file_image,fps=10):
+        self._images        = load_sliced_sprites(images,file_image)
         self._offsets       = offsets
         self._delay         = 1000 / fps
         self.start(0)
@@ -16,7 +33,7 @@ class Animation():
         self.image          = self._images[self._frame]
         self.offpt          = self._offsets[self._frame]
     def update(self,t):
-        if t - self._last_update > self._delay:
+        if t - self._last_update >= self._delay:
             self._frame += 1
             if self._frame >= len(self._images):
                 self._frame = 0
@@ -31,26 +48,55 @@ class Fighter():
         self._animation = ''
         self.animations = {}
     def __str__(self):
-        anims = " ".join("%s - %s" % (key, anim) \
+        """
+        anims = "\n\t".join("%s - %s" % (key, anim)
                     for key, anim in self.animations.items())
-        return "%s - %s : %s" % (self.health,self.location,anims)
+        """
+        if self.animations:
+            frame = "%s (%s)" % (self.state()._frame,len(self.state()._images))
+        else:
+            frame = ""
+        return "%s - %s : %s - %s" % \
+            (self.health,self.location,self._animation,frame)
+    def state(self):
+        return self.animations[self._animation]
     def update(self,t):
-        pass
+        self.state().update(t)
+    def render(self,screen):
+        screen.blit(self.state().image,self.location)
 
 class Hulk(Fighter):
     def __init__(self,location):
         Fighter.__init__(self,999,location) 
         self._animation = 'walk'
-        self.animations = {'walk':Animation([1,2,3],[1,2,3])}
+        for key, val in hulk_data.items():
+            self.animations[key] = Animation(val['rects'],
+                                             val['offpt'],
+                                             val['image'])
     def update(self,t):
-        self.animations[self._animation].update(t)
+        Fighter.update(self,t)
+
+def load_sliced_sprites(sprite_images, filename):
+    images = []
+    sheet  = pygame.image.load(os.path.join('data', filename)).convert_alpha()
+    for srect in sprite_images:
+        #simg = pygame.transform.flip(sheet.subsurface(srect), 1, 0).convert()
+        simg = sheet.subsurface(srect).convert()
+        simg.set_colorkey(simg.get_at((0, 0)), RLEACCEL)
+        images.append(simg)
+    return images
 
 def run():
     hero = Fighter(100,[10,20])
     hulk = Hulk([50,20])
     print "hero - %s" % hero
     print "hulk - %s" % hulk
-    hulk.update(101)
-    print "hulk - %s" % hulk
+    for t in xrange(0,1000,100):
+        t += 100
+        screen.blit(background, (0, 0))
+        hulk.update(t)
+        hulk.render(screen)
+        print "hulk - %s" % hulk
+        pygame.display.update()
 
 if __name__ == "__main__": run()
