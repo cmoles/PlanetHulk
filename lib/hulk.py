415544cc9472d8a98571152cebf02b5724a3e429
Chris Moles <cmoles@picobook.(none)>
2010-04-22 19:42:22 -0700

    Committer: Chris Moles <cmoles@picobook.(none)>
     On branch master
     Changes to be committed:
    
    	modified:   lib/hulk.py

--- lib/hulk.py
+++ lib/hulk.py
@@ -13,19 +13,19 @@ HULK_DATA = {
             (285, 160, 47, 65),
             (344, 159, 46, 64),],
         'offpt':[
-            (  +4,  -1),
-            (  +4,  -4),
-            (  -4,  -2),
-            (  -3,  -1),
-            (  +4,  -2),
-            (  +3,  -3),
-            (  -5,  -1),
-            (  -5,  -1),],
-       'slide':-10,
-       'image':'theincrediblehulkwu6.png'
+            (+4, -1),
+            (+4, -4),
+            (-4, -2),
+            (-3, -1),
+            (+4, -2),
+            (+3, -3),
+            (-5, -1),
+            (-5, -1),],
+        'slide':-10,
+        'image':'theincrediblehulkwu6.png'
     },
    'dash': {
-       'rects':[
+        'rects':[
             (  3, 243, 51, 45),
             ( 56, 242, 59, 48),
             (123, 242, 64, 50),
@@ -33,51 +33,48 @@ HULK_DATA = {
             (270, 246, 52, 45),
             (328, 245, 57, 47),
             (395, 245, 66, 45),
-            (469, 239, 55, 53),
-       ],
-       'offpt':[
-            ( +6,   17),
-            ( -2,   14),
-            ( -7,   12),
-            ( +1,   14),
-            ( +7,   17),
-            ( -0,   15),
-            ( -6,   17),
-            ( +3,   9),],
-       'slide':0,
-       'image':'theincrediblehulkwu6.png'
+            (469, 239, 55, 53),],
+        'offpt':[
+            (+6, 17),
+            (-2, 14),
+            (-7, 12),
+            (+1, 14),
+            (+7, 17),
+            (-0, 15),
+            (-6, 17),
+            (+3,  9),],
+        'slide':0,
+        'image':'theincrediblehulkwu6.png'
     },
    'punk': {
-       'rects':[
+        'rects':[
             (  5, 394, 51, 62),
             ( 63, 395, 47, 62),
             (117, 394, 68, 63),
             (191, 395, 47, 62),
-            (250, 394, 69, 63),
-       ],
-       'offpt':[
-            ( -6,   2),
-            ( -2,   2),
-            (  2,   1),
-            (  3,   2),
-            (  3,   1),],
-       'slide':3,
-       'image':'theincrediblehulkwu6.png'
+            (250, 394, 69, 63),],
+        'offpt':[
+            (-6, 2),
+            (-2, 2),
+            ( 2, 1),
+            ( 3, 2),
+            ( 3, 1),],
+        'slide':3,
+        'image':'theincrediblehulkwu6.png'
     },
    'stay': {
-       'rects':[
+        'rects':[
             (184,  83, 39, 64),
             (230,  83, 40, 64),
             (275,  83, 40, 64),
-            (319,  83, 41, 64),
-       ],
-       'offpt':[
-            ( +1,   0),
-            (  0,   0),
-            (  0,   0),
-            ( -1,   0),],
-       'slide':0,
-       'image':'theincrediblehulkwu6.png'
+            (319,  83, 41, 64),],
+        'offpt':[
+            (+1, 0),
+            ( 0, 0),
+            ( 0, 0),
+            (-1, 0),],
+        'slide':0,
+        'image':'theincrediblehulkwu6.png'
    }
 }
 
@@ -92,10 +89,12 @@ class Hulk(Fighter):
                                              val['image'])
     def update(self,t):
         Fighter.update(self,t)
+        """
         if self.anime() is 'jump':
             if self.state().done:
                 self.change('stay',t)
                 self.force((0,0))
+        """
         if self.anime() is 'punk':
             if self.state().done:
                 self.change('stay',t)
@@ -126,7 +125,8 @@ class Hulk(Fighter):
             self.turn(1)
             self.change('walk',t)
             self.force((.25,0))
-        if key is K_SPACE and self._animation is not 'punk':
-            self.change('punk',t)
-            self.force((0,0))
+        if key is K_SPACE:
+            if self._animation is not 'punk':
+                self.change('punk',t)
+                self.force((0,0))
 
